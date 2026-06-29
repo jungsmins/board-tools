81a5b4cb6d20b27609b99841e58eff642253c439
jungsmins <jungsmins00@gmail.com>
2026-06-29 23:37:24 +0900

    feat: RuinsCardContent에 폐허 이미지 추가

--- src/components/cartographers/RuinsCardContent.tsx
+++ src/components/cartographers/RuinsCardContent.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+import ruinsImage from '../../../public/cartographers_images/ruins.png';
+
 export default function RuinsCardContent() {
-  return <div>ruins</div>;
+  return (
+    <div className='flex flex-1 w-full items-center justify-center rounded-lg bg-[var(--color-cartographers-overlay)]'>
+      <Image
+        alt='폐허된 사원 이미지'
+        src={ruinsImage}
+        width={200}
+        height={200}
+      />
+    </div>
+  );
 }
