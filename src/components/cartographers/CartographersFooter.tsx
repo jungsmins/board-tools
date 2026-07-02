b93565c59f9520f4db377732c47a1ccc0702b1dc
jungsmins <jungsmins00@gmail.com>
2026-07-02 23:30:01 +0900

    refactor: 푸터 버튼 레이아웃을 3열 grid 정렬로 변경

--- src/components/cartographers/CartographersFooter.tsx
+++ src/components/cartographers/CartographersFooter.tsx
@@ -23,18 +23,24 @@ export default function CartographersFooter({
   return (
     <footer className='flex h-20 w-full items-center bg-[var(--color-cartographers-surface)] p-6 text-[var(--color-cartographers-ink)]'>
       <Link href='/'>홈</Link>
-      <div className='flex flex-1 justify-center gap-10'>
-        {onPrev && (
-          <Button variant='secondary' size='lg' onClick={onPrev}>
-            이전 카드
+      <div className='grid flex-1 grid-cols-3 items-center'>
+        <div className='justify-self-end'>
+          {onPrev && (
+            <Button variant='secondary' size='lg' onClick={onPrev}>
+              이전 카드
+            </Button>
+          )}
+        </div>
+        <div className='justify-self-center'>
+          <Button variant='primary' size='lg' onClick={onNext}>
+            {nextButtonLabel}
           </Button>
-        )}
-        <Button variant='primary' size='lg' onClick={onNext}>
-          {nextButtonLabel}
-        </Button>
-        <Button variant='danger' size='lg' onClick={handleReset}>
-          처음으로
-        </Button>
+        </div>
+        <div className='justify-self-start'>
+          <Button variant='danger' size='lg' onClick={handleReset}>
+            처음으로
+          </Button>
+        </div>
       </div>
     </footer>
   );
