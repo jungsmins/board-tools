d4c459dae568d54ffc0124240ffde184c42d2530
jungsmins <jungsmins00@gmail.com>
2026-08-19 00:16:30 +0900

    feat: Footer를 브랜드 다크 배경으로 리디자인

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,18 +1,11 @@
 export default function Footer() {
-  const edgeGradientBar = `
-    after:content-['']
-    after:absolute
-    after:left-0
-    after:right-0
-    after:h-1
-    after:bg-[linear-gradient(90deg,var(--color-gradient-red)_0%,var(--color-gradient-blue)_33%,var(--color-gradient-green)_66%,var(--color-gradient-yellow)_100%)]
-  `;
-
   return (
     <footer
-      className={`relative flex flex-col items-center justify-center bg-chrome py-12 after:top-0 ${edgeGradientBar}`}
+      className={
+        'relative flex flex-col items-center justify-center bg-brand-900 py-12'
+      }
     >
-      <p className='text-title text-xl'>보드툴즈</p>
+      <p className='text-xl text-surface'>보드툴즈</p>
     </footer>
   );
 }
