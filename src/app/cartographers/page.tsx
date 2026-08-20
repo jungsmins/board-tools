2ab9fa684eb1ee1e802f977f7c5a7230283c3058
jungsmins <jungsmins00@gmail.com>
2026-08-20 22:53:55 +0900

    Revert "refactor: 지도제작자들 설정/게임 진행을 별도 라우트로 분리"
    This reverts commit 6e27188eec5482601e639d5a3db0e58a01893a1d.

--- src/app/cartographers/page.tsx
+++ src/app/cartographers/page.tsx
@@ -1,15 +1,21 @@
 'use client';
 
-import { useCartographersStore } from '@/stores/cartographers';
 import SetupScreen from '@/components/cartographers/SetupScreen';
+import SeasonSplash from '@/components/cartographers/SeasonSplash';
+import PlayingScreen from '@/components/cartographers/PlayingScreen';
+import ScoringScreen from '@/components/cartographers/ScoringScreen';
+import { useCartographersStore } from '@/stores/cartographers';
 import styles from './theme.module.css';
 
 export default function CartographersPage() {
-  const { currentSeason } = useCartographersStore();
+  const { gamePhase, currentSeason } = useCartographersStore();
 
   return (
     <div className={styles[currentSeason]}>
-      <SetupScreen />
+      {gamePhase === 'setup' && <SetupScreen />}
+      {gamePhase === 'season_splash' && <SeasonSplash season={currentSeason} />}
+      {gamePhase === 'playing' && <PlayingScreen />}
+      {gamePhase === 'season_scoring' && <ScoringScreen />}
     </div>
   );
 }
