f7d55dff9a35103afe31a1e09a3148d25270cf2e
jungsmins <jungsmins00@gmail.com>
2026-09-16 00:34:37 +0900

    fix: proxy config api 추가

--- src/proxy.ts
+++ src/proxy.ts
@@ -29,6 +29,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|icon.png|.*\\.(?:png|jpg|jpeg|svg|webp|gif)$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|icon.png|.*\\.(?:png|jpg|jpeg|svg|webp|gif)$).*)',
   ],
 };
